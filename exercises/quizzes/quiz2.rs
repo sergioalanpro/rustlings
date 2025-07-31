0ccab7cb567a346e94f65a81dcdcf3b42f488297
sergioalanpro <sergioalanpro@hotmail.com>
2025-07-31 01:36:17 -0600

    rust

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -24,10 +24,28 @@ enum Command {
 }
 
 mod my_module {
+
     use super::Command;
 
     // TODO: Complete the function as described above.
     // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut output = Vec::new();
+
+        for (string, command) in input {
+            // Create the new string.
+            let new_string = match command {
+                Command::Uppercase => string.to_uppercase(),
+                Command::Trim => string.trim().to_string(),
+                Command::Append(n) => string + &"bar".repeat(n),
+            };
+
+            // Push the new string to the output vector.
+            output.push(new_string);
+        }
+
+        output
+    }
 }
 
 fn main() {
@@ -39,6 +57,7 @@ mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
     // use ???;
     use super::Command;
+    use super::my_module::transformer;
 
     #[test]
     fn it_works() {
